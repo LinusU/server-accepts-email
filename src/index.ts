6ca0efe010ff433b896c84afc716b7b8a7195812
Linus Unnebäck <linus@folkdatorn.se>
2018-06-08 12:35:14 +0200

    🎉 Limit parallel MX resolves

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,35 @@ import os = require('os')
 import util = require('util')
 
 import pCatchIf = require('p-catch-if')
+import pLimit = require('p-limit')
 import pSleep = require('p-sleep')
 
 import Client from './client'
 import Manager from './manager'
 
 const debug = require('debug')('server-accepts-email') as (s: string) => void
-const resolveMx = util.promisify(dns.resolveMx)
 
 const globalManager = new Manager()
+
+const resolveMx = util.promisify(dns.resolveMx)
+const resolveMxLimit = pLimit(256)
+
+async function getMailServers (hostname: string): Promise<string[]> {
+  return resolveMxLimit(async () => {
+    debug(`Resolving MX records for "${hostname}"`)
+    const mxRecords = await resolveMx(hostname).catch(pCatchIf(err => (err.code === 'ENOTFOUND' || err.code === 'ENODATA'), () => []))
+    debug(`Got ${mxRecords.length} record${mxRecords.length === 1 ? '' : 's'} for "${hostname}"`)
+
+    /* https://en.wikipedia.org/wiki/MX_record#Priority
+    * The MX priority determines the order in which the servers
+    * are supposed to be contacted: The servers with the highest
+    * priority (and the lowest preference number) shall be tried
+    * first. Node, however, erroneously labels the preference number
+    * "priority". Therefore, sort the addresses by priority in
+    * ascending order, and then contact the first exchange. */
+    return mxRecords.sort((lhs, rhs) => lhs.priority - rhs.priority).map(a => a.exchange)
+  })
+}
 
 interface TestServerOptions {
   senderAddress: string
@@ -37,30 +57,18 @@ async function testServer (client: Client, email: string, { senderAddress, handl
 }
 
 export = async function serverAcceptsEmail (email: string, options: { senderDomain?: string, senderAddress?: string } = {}) {
-  const server = email.split('@')[1]
+  const hostname = email.split('@')[1]
+  const servers = await getMailServers(hostname)
 
-  debug(`Resolving MX records for "${server}"`)
-  const mxRecords = await resolveMx(server).catch(pCatchIf(err => (err.code === 'ENOTFOUND' || err.code === 'ENODATA'), () => []))
-  debug(`Got ${mxRecords.length} record${mxRecords.length === 1 ? '' : 's'} for "${server}"`)
-
-  if (mxRecords.length === 0) {
+  if (servers.length === 0) {
     return false
   }
 
   const senderDomain = (options.senderDomain || os.hostname())
   const senderAddress = (options.senderAddress || `test@${senderDomain}`)
 
-  /* https://en.wikipedia.org/wiki/MX_record#Priority
-   * The MX priority determines the order in which the servers
-   * are supposed to be contacted: The servers with the highest
-   * priority (and the lowest preference number) shall be tried
-   * first. Node, however, erroneously labels the preference number
-   * "priority". Therefore, sort the addresses by priority in
-   * ascending order, and then contact the first exchange. */
-  const sortedServers = mxRecords.sort((lhs, rhs) => lhs.priority - rhs.priority).map(a => a.exchange)
-
   let lastError: Error | null = null
-  for (const server of sortedServers) {
+  for (const server of servers) {
     try {
       return await globalManager.withClient(server, senderDomain, (client) => {
         return testServer(client, email, { senderAddress, handleGraylisting: true })
